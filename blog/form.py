aada677ca37f77b3850e051e454fd71343fd3399
likelionatky <lego7407@gmail.com>
2019-02-14 14:33:20 +0900

    form change

--- blog/form.py
+++ blog/form.py
@@ -12,4 +12,4 @@ class BlogPost(forms.Form):
     files = forms.FileField()
     url = forms.URLField()
     words = forms.CharField(max_length=200)
-    max_number = forms.ChoiceField(choices=[('1','one'),('2','two'),('3','three')])
+    max_number = forms.ChoiceField(choices=[('1','one'),('2','two'),('3','three'),('4','four')])
